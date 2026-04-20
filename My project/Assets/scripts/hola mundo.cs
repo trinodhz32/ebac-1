b0c890d73fd71004e8db68426bba77dd531b8248
trinodhz32 <trinodhz32@gmail.com>
2026-04-20 15:32:27 -0600

    Practica-Modulo-5

--- My project/Assets/scripts/hola mundo.cs
+++ My project/Assets/scripts/hola mundo.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = 0;
+        print("algo paso");
+
+        Debug.LogWarning("Algo mediana mente mal");
+        Debug.LogError("Algo salio muy mal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       // x = x + 1;
+        // Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
     }
-}
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("gola desde fixed update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("hola desde late update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto a sido habilitado");
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitaado");
+    }
+} 
